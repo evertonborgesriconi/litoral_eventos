8cb527df17924d05b264061a9493ba6b445fa71d
Everton Riconi <evertonriconi@gmail.com>
2022-10-17 20:03:48 -0300

    nav

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,17 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-
+        criador: {}
     },
     getters: {
 
     },
     mutations: {
+
+        pegaCriador (state, payload) {
+            state.criador = payload
+        }
+
 
     },
     actions: {
--- src/views/Admin/DashbordView.vue
+++ src/views/Admin/DashbordView.vue
@@ -1,57 +1,82 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid"></div>
+  <section>
+    <nav>
+      <div class="btn1">Bem vindo!! {{ criador.name }}</div>
+
+      <button type="submit" class="btn">Meu perfil</button>
     </nav>
-  </div>
+    {{ criador }}
+  </section>
 </template>
 
 <script>
 import { adminApi } from "../../../http/index";
 import router from "@/router";
+//import SpinnerApp from "../../components/SpinnerApp.vue";
+
 export default {
   name: "DashbordView",
-  props: ['username'],
+  props: ["username"],
+  //   components: { SpinnerApp },
   data() {
     return {
-      criador: {
-        email: "",
-        password: "",
-      },
-
-      errors: "",
-      loading: true,
+      loading: false,
     };
-    
   },
   created() {
     this.getCriador();
   },
 
+  computed: {
+    criador() {
+      return this.$store.state.criador;
+    },
+  },
   methods: {
     getCriador() {
       this.loading = false;
-      const criador_id = sessionStorage.getItem('id_criador');
+      const criador_id = sessionStorage.getItem("id_criador");
 
       if (criador_id) {
-         adminApi
-        .get(`criador/${criador_id}`, this.criador)
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error.request.response);
-        });
-      }else{
+        adminApi
+          .get(`criador/${criador_id}`, this.criador)
+          .then((response) => {
+            console.log(response.data);
+            this.$store.commit("pegaCriador", response.data);
+            this.loading = true;
+          })
+          .catch((error) => {
+            console.log(error.request.response);
+          });
+      } else {
         router.push({ path: "/login" });
       }
-
-
-     
     },
   },
 };
 </script>
 
 <style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #4b48480f;
+  width: 100%;
+  font-size: 2.9rem;
+  font-weight: bold;
+  color: #18c07a;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+.btn {
+  background-color: #18c07a;
+  color: #ffffff;
+  border-radius: 5%;
+ 
+  
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+}
 </style>
